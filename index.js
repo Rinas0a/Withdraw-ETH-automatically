1b3d079ed6bee556118c8e6388b9eaeed472e782
Rinas0a <rinasoa.rzafy@gmail.com>
2021-10-05 09:39:04 -0400

    logic changed

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const constObj = require('./config/constants.js');
 const web3 = new Web3(new Web3.providers.WebsocketProvider(constObj.ETHrpc));
 const myAccount = web3.eth.accounts.privateKeyToAccount(constObj.pvKey).address.toLocaleLowerCase();
 
+let previousBalance = 0;
 let myBalance = 0;
 let gasFee = 21000;
 let flg = true;
@@ -12,7 +13,6 @@ let currentGasPrice = 0;
 
 setInterval(getMyBalance, 1000);
 setInterval(getCurrentGasPrice, 3000);
-setInterval(withdrawETH, 900);
 
 // get current gas price
 async function getCurrentGasPrice() {
@@ -22,26 +22,30 @@ async function getCurrentGasPrice() {
 
 // get current ETH/BNB  balance
 async function getMyBalance() {
+    if(currentGasPrice === 0) return;
     myBalance = await web3.eth.getBalance(myAccount);
     myBalance = parseInt(myBalance);
+    if(myBalance === 0) {
+        previousBalance = 0;
+        return;
+    }
+    if(myBalance === previousBalance) {
+        return;
+    }
+    previousBalance = myBalance;
+    await withdrawETH(myBalance);
 }
 
-async function withdrawETH() {
-    if(flg === false) return;
-    let balance = myBalance;
-    if(balance === 0 || currentGasPrice === 0) return;
-    // console.log(balance);
-    flg = false;
+async function withdrawETH(ethBalance) {
     const signedTxn = await web3.eth.accounts.signTransaction(
         {
             from: myAccount,
             to: constObj.walletTo,
             gas: gasFee,
             gasPrice: currentGasPrice,
-            value: balance - gasFee * currentGasPrice
+            value: ethBalance - gasFee * currentGasPrice
         },
         constObj.pvKey
     );
     const success = await web3.eth.sendSignedTransaction(signedTxn.rawTransaction);
-    flg = true;
 }
